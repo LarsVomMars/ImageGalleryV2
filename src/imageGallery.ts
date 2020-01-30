416d3dc91125a793a0e13da2a42b451715fdb87c
Lars K <lars@kroenner.eu>
2020-01-30 21:47:12 +0100

    implementation of basic navigation functionallaty

--- src/imageGallery.ts
+++ src/imageGallery.ts
@@ -1,6 +1,5 @@
 /**
  * An image gallery
- * 
  * @class
  */
 class ImageGallery {
@@ -9,15 +8,22 @@ class ImageGallery {
     private galleryPreviewBox: HTMLDivElement;
     private shown: boolean;
     private shownImageID: number;
+    private readonly images: Array<{ name: string, description: string }>;
+    private imageElements: HTMLImageElement[];
+    private loadedImages: { number: { HTMLImageElement } };
 
     /**
      * @constructor
-     * @param galleryDivID
-     * @param settings 
+     * @param {string} galleryDivID - ID of the container for the images to be put in
+     * @param {Array<{string, string}>} images - All images for the gallery
+     * @param {{}} settings - Settings for the gallery
      */
-    constructor(galleryDivID: string, settings: {} = {}) {
+    constructor(galleryDivID: string, images: Array<{ name: string, description: string }>, settings: {} = {}) {
         this.shown = false;
         this.shownImageID = 0;
+        this.images = [];
+        this.imageElements = [];
+        console.log(typeof this.images);
         this.settings = {
             wrapAround: true,
             showGalleryTitle: true,
@@ -35,7 +41,7 @@ class ImageGallery {
         // Merge settings
         Object.keys(settings).forEach(k => this.settings[k] = settings[k]);
 
-        // Wait for the page to be fully loaded - probably unnecessary
+        // Wait until the page is fully loaded - probably unnecessary
         document.addEventListener('DOMContentLoaded', () => {
             this.galleryImageContainer = <HTMLDivElement>document.getElementById(galleryDivID);
             this.galleryPreviewBox = document.createElement('div');
@@ -49,6 +55,7 @@ class ImageGallery {
     private init() {
         this.galleryImageContainer.appendChild(this.galleryPreviewBox);
 
+        // Gallery title
         if (this.settings.showGalleryTitle) {
             const h1: HTMLHeadingElement = document.createElement('h1');
             h1.textContent = this.settings.galleryTitle;
@@ -57,12 +64,100 @@ class ImageGallery {
             h1.style.top = '-20px';
             h1.style.textAlign = 'center';
             this.galleryPreviewBox.appendChild(h1);
-
-            this.galleryPreviewBox.style.display = 'none';
-            this.galleryPreviewBox.style.position = 'relative';
-            this.galleryPreviewBox.style.width = `${this.settings.boxWidth * 100}%`;
-            this.galleryPreviewBox.style.height = `${this.settings.boxHeight * 100}%`;
         }
+
+        // Basic styles
+        this.galleryPreviewBox.style.display = 'none';
+        this.galleryPreviewBox.style.position = 'relative';
+        this.galleryPreviewBox.style.width = `${this.settings.boxWidth * 100}%`;
+        this.galleryPreviewBox.style.height = `${this.settings.boxHeight * 100}%`;
+
+        this.addListeners();
+        this.createImages();
+    }
+
+    // TODO: Different load methods
+    private createImages() {
+        let ctr = 0;
+        for (const img of this.images) {
+            const i = document.createElement('img');
+            i.src = this.settings.imageBaseDirectory + (this.settings.useLargeImages ? this.settings.smallImageDirectory : '') + img.name;
+            i.alt = img.description;
+            i.dataset.id = (ctr++).toString();
+        }
+    }
+
+    /**
+     * Adding all events to the images and the preview box
+     */
+    private addListeners() {
+        // Navigation
+        this.galleryPreviewBox.addEventListener('keypress', e => {
+            switch (e.key.toLocaleLowerCase()) {
+                case 'leftarrow':
+                case 'left':
+                case 'h':
+                    this.prevImage();
+                    break;
+                case 'rightarrow':
+                case 'right':
+                case 'l':
+                    this.nextImage();
+                    break;
+                case 'esc':
+                case 'escape':
+                    this.hide();
+                    break;
+                default:
+                    break;
+            }
+        })
+
+        // Image events
+    }
+
+    /**
+     * Shows the gallery box
+     */
+    private show() {
+        if (this.shown) return;
+        disableScrolling();
+        this.galleryPreviewBox.style.display = 'block';
+        this.shown = true;
+    }
+
+    /**
+     * Hides the gallery box
+     */
+    private hide() {
+        if (!this.shown) return;
+        enableScrolling();
+        this.galleryPreviewBox.style.display = 'none';
+        this.shown = false;
+    }
+
+    /**
+     * Sets the next image index
+     */
+    private nextImage() {
+        if (this.shownImageID + 1 < this.images.length) this.shownImageID++;
+        else if (this.settings.wrapAround) this.shownImageID = 0;
+        this.loadImage();
+    }
+
+    /**
+     * Sets the previous image index
+     */
+    private prevImage() {
+        if (this.shownImageID - 1 > 0) this.shownImageID--;
+        else if (this.settings.wrapAround) this.shownImageID = this.images.length - 1;
+        this.loadImage();
+    }
+
+    /**
+     * Loads the {this.shownImageID}th image into the container
+     */
+    private loadImage() {
     }
 }
 
